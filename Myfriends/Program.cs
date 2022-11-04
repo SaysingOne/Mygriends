b1891c0a1a7090f5a7683882fc9e9332a44faa3f
stanislavzabin <sats271988@gmail.com>
2022-11-04 15:06:04 +0200

    First commit

---
+++ Myfriends/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;       
+string name = "Protyk";
+double birthday = 04.11;
+int age = 33;
+bool smoking = true;
+
+Console.ReadKey();
+Console.Clear();
+
+Console.WriteLine($"Имя: {name}, День рождения: {birthday}, Возраст: {age}old, Курит: {smoking}");
+
+Console.ReadKey();
+Console.Clear();
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+name = "Tywa";
+birthday = 06.04;
+age = 32;
+smoking = true;
+
+Console.WriteLine($"Имя: {name}, День рождения: {birthday}, Возраст: {age}old , Курит: {smoking}");
+
+Console.ReadKey();
+Console.Clear();
+
+name = "Ksusha";
+birthday = 08.04;
+age = 30;
+smoking = false;
+
+Console.WriteLine($"Имя: {name}, День рождения: {birthday}, Возраст: {age}old, Курит: {smoking}");
+
+Console.ReadKey();
+Console.Clear();
+
+name = "Vinni";
+birthday = 30.10;
+age = 33;
+smoking = true;
+
+Console.WriteLine("Имя: {0} День рождения: {2} Возраст: {1}old Курит: {3}", name, age, birthday, smoking);
+
+Console.ReadKey();
+Console.Clear();
