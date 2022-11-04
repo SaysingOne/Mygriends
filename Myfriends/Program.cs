c7b3fa67976c7cd8ac7138c747ce1df1eb1be55f
SaysingOne <sats271988@gmail.com>
2022-11-04 15:39:43 +0200

    change Program.cs

--- Myfriends/Program.cs
+++ Myfriends/Program.cs
@@ -25,7 +25,7 @@ Console.Clear();
 
 name = "Ksusha";
 birthday = 08.04;
-age = 30;
+age = 31;
 smoking = false;
 
 Console.WriteLine($"Имя: {name}, День рождения: {birthday}, Возраст: {age}old, Курит: {smoking}");
